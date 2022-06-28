38646b54d239644ec87d2d5767e95e3dd8094bf2
DeEzUzZa-7012 <108201261+DeEzUzZa-7012@users.noreply.github.com>
2022-06-28 11:18:29 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -10,7 +10,7 @@ class Translation(object):
 Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
 
 For More Details Click Help Button Below..
-@moviesbizzbotz
+@malayali00l
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +43,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@moviesbizzbotz
+@malayali00l
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/crizzbizzyt">CrizzBizz YT</a></i></b>
+<b>➥ Join Group</b> : <b><i><a href="https://t.me/MMCINEMACHAKKU">Cinema Club</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Join Channel</b> : <i><a href="https://t.me/+yP9ckmsoGF1kNDNl">Click Me</a></i>
 """
